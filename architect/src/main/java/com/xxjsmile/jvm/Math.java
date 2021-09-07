3dfb90fcdb397824e62de6bf816fe3192678bb90
xiaoxiangjie <xxj_jobs@163.com>
2021-09-07 22:52:42 +0800

    add:file

---
+++ architect/src/main/java/com/xxjsmile/jvm/Math.java
@@ -0,0 +1,14 @@
+package com.xxjsmile.jvm;
+
+public class Math {
+
+
+    public static void main(String[] args) {
+        String str = "";
+        test123();
+        
+    }
+
+    private static void test123() {
+    }
+}
