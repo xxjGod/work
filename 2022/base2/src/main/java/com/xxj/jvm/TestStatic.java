6914b000d9bebe1d832136275f397da3eb556de9
xiaoxiangjie <xiaojie.xiao@17zuoye.com>
2021-12-31 16:14:57 +0800

    add:2022 start

---
+++ 2022/base2/src/main/java/com/xxj/jvm/TestStatic.java
@@ -0,0 +1,25 @@
+package com.xxj.jvm;
+
+import java.io.Serializable;
+
+/**
+ * @description:
+ * @author: xiangjie.xiao
+ * @create: 2021-12-31 11:26
+ **/
+public class TestStatic implements Serializable {
+
+
+    public String m = "m1";
+    public final String n = "n1";
+    private static String hello = "hello1";
+    private static final String HELLO = "HELLO1";
+
+    public void test1() {
+        String va = "xxjsmile test1";
+    }
+
+    public static void test2() {
+        String va = "xxjsmile test2";
+    }
+}
